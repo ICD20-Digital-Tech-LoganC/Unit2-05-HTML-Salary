014373e4a2af315e5e79124d9461a36b0da6138e
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-03-26 14:55:42 +0000

    finished product

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Logan Connors
+// Created on: Mar 25 2024
 // This file contains the JS functions for index.html
+"use strict";
+
+// This function calculates the salary of the user
+function enterClicked() {
+  // input
+  let hours_worked = parseInt(document.getElementById('hours_worked').value);
+  let hourly_Rate = parseInt(document.getElementById('hourly_Rate').value);
+
+  // process
+  let preTaxTotal = hours_worked * hourly_Rate;
+  let afterTaxTotal = 0.2005 * preTaxTotal;
+
+  // output
+  document.getElementById('answers2').innerHTML = 'Your pay will be $' + preTaxTotal;
+  document.getElementById('answers').innerHTML = 'The government will take $' + afterTaxTotal;
+}
